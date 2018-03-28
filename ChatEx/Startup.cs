711bac6b695ab73334ab110cac108064731389ff
JHarrell1211 <julian1211@gmail.com>
2018-03-28 10:23:35 -0400

    first commit

---
+++ ChatEx/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ChatEx.Startup))]
+namespace ChatEx
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
